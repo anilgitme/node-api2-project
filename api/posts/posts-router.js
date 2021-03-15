12d3542d7e91b152e7e8b91a7018bfb29ce4f0d6
Anil_Ramcharran <anil.gitme@gmail.com>
2021-03-14 21:22:33 -0400

    progress made 2 get requests working on the rest

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,36 @@
 // implement your posts router here
+const express = require('express');
+const router = express.Router();
+const Post = require('./posts-model');
+
+//get post
+router.get('/', (req, res) => {
+    Post.find()
+        .then((data) => {
+            res.status(200).json(data);
+        })
+        .catch(error => {
+            res.status(500).json({
+                message: 'The posts information could not be retrieved'
+            })
+        })
+})
+
+//retrieve posts by id
+
+router.get('/:id', (req, res) => {
+    Post.findById(req.params.id)
+        .then((post) => {
+            if (!post) {
+                res.status(404)
+                    .json({ message: 'The post with the specified ID does not exist' })
+            } else {
+                res.status(200).json(post); //found post!
+            }
+        })
+        .catch((error) => {
+            res.status(500).json({
+                message: 'The posts information could not be retrieved'
+            })
+        })
+})
